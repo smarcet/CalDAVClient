d821789e1a522a4273332523ca0e9143d5959d84
Edward Akerboom <github@infostreams.net>
2018-03-02 13:52:36 +0100

    Made tests succeed on both iCloud and SabreDAV
    The tests were very tailored for iCloud. I adjusted them to run on more generic CalDav servers as well.

--- tests/FacadeTest.php
+++ tests/FacadeTest.php
@@ -26,12 +26,29 @@ final class FacadeTest extends PHPUnit_Framework_TestCase
      */
     private static $client;
 
+    /**
+     * @var string URL of the calendar that was created during the test
+     */
+    private static $calendar_created_by_phpunit = "";
+
+    /**
+     * @var \CalDAVClient\Facade\Responses\EventCreatedResponse
+     */
+    private static $event_created_by_phpunit    = null;
+
+    private static $calendar_home = null;
+
+    public function setUp() {
+        parent::setUp();
+    }
+
     public static function setUpBeforeClass()
     {
        self::$client = new CalDavClient(
-            getenv('CALDAV_SERVER_URL'),
-            getenv('USER_EMAIL'),
-            getenv('USER_PASSWORD')
+            getenv('CALDAV_SERVER_HOST') . getenv('CALDAV_SERVER_PATH'),
+            getenv('USER_LOGIN'),
+            getenv('USER_PASSWORD'),
+            getenv('AUTHTYPE')
         );
     }
 
@@ -40,92 +57,108 @@ final class FacadeTest extends PHPUnit_Framework_TestCase
         // do sth after the last test
     }
 
+    private function getCalendarUrl() {
+        return
+            getenv('CALDAV_SERVER_HOST') .
+            getenv('CALDAV_SERVER_PATH') .
+            getenv('CALDAV_CALENDAR_HOME') .
+            getenv('CALDAV_TEST_CALENDAR_PATH');
+    }
+
     function testIsValidServer(){
-        $this->assertTrue(self::$client ->isValidServer());
+        $this->assertTrue(self::$client->isValidServer());
     }
 
     function testPrincipal(){
+        $principals = self::$client->getUserPrincipal();
+        $responses  = $principals->getResponses();
 
-        $res    = self::$client ->getUserPrincipal();
-        $url    = $res->getPrincipalUrl();
+        foreach ($responses as $res) {
+            $url = $res->getPrincipalUrl();
 
-        $this->assertTrue(!empty($url));
-        echo sprintf('principal url is %s', $url).PHP_EOL;
-        return $url;
+            $this->assertTrue(!empty($url), "Principal URL is empty");
+            return $url;
+        }
     }
 
     function testCalendarHomes(){
+        $caldav_host = getenv("CALDAV_SERVER_HOST");
+        $caldav_path = getenv("CALDAV_SERVER_PATH");
+
         $principal_url = $this->testPrincipal();
-        $res    = self::$client->getCalendarHome($principal_url);
+        $res    = self::$client->getCalendarHome($caldav_host . $principal_url);
         $url    = $res->getCalendarHomeSetUrl();
 
-        $this->assertTrue(!empty($url));
-        $host = $res->getRealCalDAVHost();
-        echo sprintf('calendar home is %s', $url).PHP_EOL;
-        echo sprintf('host is %s', $host).PHP_EOL;
-        return $url;
+        $this->assertTrue(!empty($url), "Calendar home URL is empty");
+        // $host = $res->getRealCalDAVHost();
+        // echo sprintf('calendar home is %s', $url).PHP_EOL;
+        // echo sprintf('host is %s', $caldav_host).PHP_EOL;
+
+        // first, ensures that the 'home' path is relative to the CalDav server
+        // (this differs between servers)
+        $path_without_prefix = $url;
+        if (strpos($path_without_prefix, $caldav_host) === 0) {
+            $path_without_prefix = substr($path_without_prefix, strlen($caldav_host));
+        }
+        if (strpos($path_without_prefix, $caldav_path) === 0) {
+            $path_without_prefix = substr($path_without_prefix, strlen($caldav_path));
+        }
+
+        // then, turn the URL into an absolute URL so that we always know what to expect
+        self::$calendar_home = $caldav_host . $caldav_path . $path_without_prefix;
     }
 
     function testGetCalendars(){
-        $calendar_home = $this->testCalendarHomes();
+        $calendar_home = self::$calendar_home;
+
         $res   = self::$client->getCalendars($calendar_home);
-        $this->assertTrue($res->isSuccessFull());
-        $this->assertTrue(count($res->getResponses()) > 0);
+        $this->assertTrue($res->isSuccessFull(), "GetCalendars request not successful");
+        $this->assertTrue(count($res->getResponses()) > 0, "Request returned zero responses");
         return $res;
     }
 
     function testGetCalendar(){
-        $res  = self::$client->getCalendar(getenv('CALDAV_SERVER_URL').'/8244464267/calendars/openstack-summit-sidney-2017/');
-        $this->assertTrue($res->isSuccessFull());
-        $this->assertTrue(!empty($res->getDisplayName()));
-        $this->assertTrue(!empty($res->getSyncToken()));
+        $res  = self::$client->getCalendar($this->getCalendarUrl());
+        $this->assertTrue($res->isSuccessFull(), "Calendar request not successful");
+        $this->assertTrue(!empty($res->getDisplayName()), "Display name not set");
+        $this->assertTrue(!empty($res->getSyncToken()), "Sync-token empty");
+        return $res;
     }
 
     function testSyncCalendar(){
+        $cal = $this->testGetCalendar();
 
         $res    = self::$client->getCalendarSyncInfo(
-            getenv('CALDAV_SERVER_URL').'/8244464267/calendars/openstack-summit-sidney-2017/',
-            "FT=-@RU=8546e45e-a9f6-4f20-b6a2-7637f4783d8f@S=169");
+            $this->getCalendarUrl(),
+            $cal->getSyncToken());
         $this->assertTrue($res->isSuccessFull());
         $this->assertTrue(!empty($res->getSyncToken()));
     }
 
     function testCreateCalendar(){
+        $home = self::$calendar_home;
 
-        $res = self::$client->createCalendar(
-            getenv('CALDAV_SERVER_URL').'/8244464267/calendars/',
+        $link = self::$client->createCalendar(
+            $home,
             new MakeCalendarRequestVO(
-                'openstack-summit-sidney-2017',
+                null, // means: generate a unique name
                 'OpenStack Sidney Summit Nov 2017',
                 'Calendar to hold Summit Events',
                 new DateTimeZone('Australia/Sydney')
             )
         );
 
-        $this->assertTrue(!empty($res));
-    }
-
-    function testDeleteCalendar(){
-
-        $calendar_url =  'https://p01-caldav.icloud.com:443/8244464267/calendars/openstack-summit-sidney-2017';
-        $res  = self::$client->getCalendar($calendar_url);
-
-        $res = self::$client->deleteCalendar
-        (
-            $calendar_url,
-            ""
-        );
-
-        $this->assertTrue(!empty($res));
+        $this->assertTrue(!empty($link));
+        self::$calendar_created_by_phpunit = $link . '/';
     }
 
     function testCreateEvent(){
         $res = self::$client->createEvent(
-            getenv('CALDAV_SERVER_URL').'/8244464267/calendars/0f9ba5e9072576c6fab990b8f813b4e0/',
+            self::$calendar_created_by_phpunit,
             new EventRequestVO(
-                'openstack-summit-sidney-2017',
+                "test-event-" . md5(microtime(true)),
                 'test event 4',
-            'test event',
+                'test event',
                 'test event',
                 new DateTime('2017-11-01 09:00:00'),
                 new DateTime('2017-11-01 10:30:00'),
@@ -134,10 +167,11 @@ final class FacadeTest extends PHPUnit_Framework_TestCase
         );
 
         $this->assertTrue($res->isSuccessFull());
+        self::$event_created_by_phpunit = $res;
     }
 
     function testUpdateEvent(){
-        $uid = 'ad82cdfe9da1d7b8c840c3acfa65db18';
+        $uid = self::$event_created_by_phpunit->getUid();
         //$etag = "C=150@U=8546e45e-a9f6-4f20-b6a2-7637f4783d8f";
 
         $dto =  new EventRequestVO(
@@ -150,18 +184,8 @@ final class FacadeTest extends PHPUnit_Framework_TestCase
             new DateTimeZone('Australia/Sydney')
         );
         $dto->setUID($uid);
-        $res = self::$client->updateEvent(getenv('CALDAV_SERVER_URL').'/8244464267/calendars/0f9ba5e9072576c6fab990b8f813b4e0/',
+        $res = self::$client->updateEvent(self::$calendar_created_by_phpunit,
             $dto
-        );
-
-        $this->assertTrue($res->isSuccessFull());
-    }
-
-    function testDeleteEvent(){
-        $uid  = 'd738bd4a675f4b60cf664b88ce7f0659';
-
-        $res = self::$client->deleteEvent(getenv('CALDAV_SERVER_URL').'/8244464267/calendars/0f9ba5e9072576c6fab990b8f813b4e0/',
-            $uid
         );
 
         $this->assertTrue($res->isSuccessFull());
@@ -169,7 +193,7 @@ final class FacadeTest extends PHPUnit_Framework_TestCase
 
     function testGetEventByUrl(){
 
-        $event_url = 'https://p01-caldav.icloud.com:443/8244464267/calendars/openstack-summit-sidney-2017/d7a2387264bfa1a619c37a593e94204a.ics';
+        $event_url = self::$event_created_by_phpunit->getResourceUrl();
 
         $v_card = self::$client->getEventVCardBy($event_url);
 
@@ -177,11 +201,37 @@ final class FacadeTest extends PHPUnit_Framework_TestCase
     }
 
     function testGetEventsByUrl(){
-        $event_url  = '/8244464267/calendars/openstack-summit-sidney-2017/0df083912b476631bf677c140ad4740b.ics';
+        $event_url = self::$event_created_by_phpunit->getResourceUrl();
 
-        $res = self::$client->getEventsBy('https://p01-caldav.icloud.com:443/8244464267/calendars/openstack-summit-sidney-2017/',
+        $res = self::$client->getEventsBy(self::$calendar_created_by_phpunit,
             [$event_url]);
 
         $this->assertTrue($res->isSuccessFull());
+    }
+
+    function testDeleteEvent(){
+        $uid  = self::$event_created_by_phpunit->getUid();
+
+        $res = self::$client->deleteEvent(self::$calendar_created_by_phpunit,
+            $uid
+        );
+
+        $this->assertTrue($res->isSuccessFull());
+    }
+
+    function testDeleteCalendar(){
+        $host = getenv("CALDAV_SERVER_HOST");
+
+        $calendar_url = $host . (str_replace($host, "", self::$calendar_created_by_phpunit));
+
+        $res  = self::$client->getCalendar($calendar_url);
+
+        $res = self::$client->deleteCalendar
+        (
+            $calendar_url,
+            ""
+        );
+
+        $this->assertTrue(!empty($res));
     }
 }
